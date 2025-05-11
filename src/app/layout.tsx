b468d9996569fb9b08b12d1267a1f6180477d9e4
Amr Shawki <jokydokyduck@gmail.com>
2025-05-12 01:40:22 +0300

    feat: created navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
+        <Navbar />
         <main className="min-h-screen flex flex-col lg:p-8 p-4">
           {children}
         </main>
