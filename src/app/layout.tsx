4ee2b8e246902a0782e8bc558679e3518f9fc0e5
Amr_Shawki <74866082+vitaminsofsalem@users.noreply.github.com>
2025-05-15 16:46:44 +0300

    feat: created footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
         <main className="min-h-screen flex flex-col max-w-800 lg:p-8 p-4">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center text-2xl w-full text-center">
+      <span className="p-4">
+        © 2025 Youssef Tohamy — crafted with care. All rights reserved.
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
